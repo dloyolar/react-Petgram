4c25b69a50d8b1570fa387719296a42f9616f2b1
dloyolar <dieegolr@gmail.com>
2022-02-09 21:11:14 -0300

    [REFACTOR] get data from service with graphql

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+
+import { useQuery, gql } from '@apollo/client';
+
 import { PhotoCard } from '../PhotoCard';
 
+const getPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(getPhotos);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error to get data</p>;
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7].map((id) => (
-        <PhotoCard key={id} id={id} />
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
       ))}
     </ul>
   );
