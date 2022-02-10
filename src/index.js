ad2e6e952d729adc4800b28e73d4d8e7f4a50a83
dloyolar <dieegolr@gmail.com>
2022-02-09 21:09:37 -0300

    [ADD] use apollo in main app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { App } from './App';
 
-ReactDOM.render(
-  <App />,
+const client = new ApolloClient({
+  uri: 'https://petgram-server-dloyolar.vercel.app/graphql',
+  cache: new InMemoryCache(),
+});
 
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 );
