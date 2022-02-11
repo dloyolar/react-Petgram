d7a1c26c60eac3a13c8cd72f7b55307f655b6af2
dloyolar <dieegolr@gmail.com>
2022-02-11 13:15:31 -0300

    [REFACTOR] reuse Routes component in App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 
-import { ListOfCategories } from './components/ListOfCategories';
-import { ListOfPhotoCards } from './components/ListOfPhotoCards';
-import { Logo } from './components/Logo';
-import { PhotoCardWithQuery } from './components/PhotoCardWithQuery';
+import { MainRoutes } from './routes/index';
 import { GlobalStyle } from './styles/GlobalStyles';
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get('detail');
-
   return (
     <>
       <GlobalStyle />
-      <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery id={detailId} />
-      ) : (
-        <>
-          <ListOfCategories />
-          <ListOfPhotoCards categoryId={1} />
-        </>
-      )}
+      <MainRoutes />
     </>
   );
 };
