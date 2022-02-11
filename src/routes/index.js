7443de25c259e615300a53c3ac8f49b6b5d0ea04
dloyolar <dieegolr@gmail.com>
2022-02-11 13:41:13 -0300

    [ADD] use Layout in Rotues file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Detail } from '../pages/Detail';
 import { Home } from '../pages/Home';
-import { Logo } from '../components/Logo/index';
+
+import { Layout } from '../Layout/Layout';
 
 export const MainRoutes = () => {
   return (
     <BrowserRouter>
-      <Logo />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/pet/:id" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/pet/:id" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
