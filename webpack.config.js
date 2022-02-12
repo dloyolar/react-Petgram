7295f7976d8e83c100a812e5f08202c15ab1d9d0
dloyolar <dieegolr@gmail.com>
2022-02-12 18:00:02 -0300

    [ADD] PWA and Workbox

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const path = require('path');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -13,6 +17,53 @@ module.exports = {
   plugins: [
     new HtmlWebackPlugin({
       template: './src/index.html',
+    }),
+    new WebpackPwaManifestPlugin({
+      filename: 'manifest.webmanifest',
+      name: 'Petgram - Your pet photos app',
+      shortname: 'Petgram -🐱',
+      description: 'Find pet photos and use it as wallpaper',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      background_color: '#456BD9',
+      theme_color: '#456BD9',
+      prefer_related_applications: true,
+      icons: [
+        {
+          src: path.resolve('src/assets/icondog.png'),
+          size: '1024x1024',
+          sizes: [96, 128, 144, 192, 256, 384, 512],
+          purpose: 'maskable any',
+          destination: path.join('Icons'),
+          ios: true,
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.cloudinary.com|images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-dloyolar.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          },
+        },
+      ],
     }),
   ],
   module: {
