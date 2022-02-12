6f539dde1c8f139e10421f9a29740ccf3551bc52
dloyolar <dieegolr@gmail.com>
2022-02-12 13:24:14 -0300

    [ADD] spinner loader from loading.io

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Spinner } from './styles';
+
+export const SpinnerLoader = () => {
+  return (
+    <Spinner>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </Spinner>
+  );
+};
