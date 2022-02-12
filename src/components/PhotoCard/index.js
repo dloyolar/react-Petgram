520baf32c58185ef6745c5764196861b4cd55ec2
dloyolar <dieegolr@gmail.com>
2022-02-12 15:56:55 -0300

    [REFACTOR] use token from sessionStorage to getPhotos and data

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useMutationToogleLike } from '../../hooks/useMutationToggleLike';
 import { useNearScreen } from '../../hooks/useNearScreen';
 import { FavButton } from '../FavButton';
@@ -10,15 +9,12 @@ import { ImgWrapper, Img, Article } from './styles';
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
-  const key = `like-${id}`;
-  const [liked, setLiked] = useLocalStorage(key, false);
   const [toggleLike] = useMutationToogleLike();
 
   const handleFavClick = () => {
-    !liked && toggleLike({ variables: { input: { id: id } } });
-    setLiked(!liked);
+    toggleLike({ variables: { input: { id: id } } });
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { App } from './App';
 import { AppProvider } from './Context';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'https://petgram-server-dloyolar.vercel.app/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = sessionStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  onError: onError(({ networkError }) => {
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token');
+      window.location = '/user';
+    }
+  }),
 });
 
 ReactDOM.render(
